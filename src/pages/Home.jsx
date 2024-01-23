50676fd95a79c1091cc10bcd3a11c484dfaee3b8
Jarrod Bogard <jarrodbogard@gmail.com>
2024-01-23 11:06:33 -0500

    - add GET fetch to homepage for retreiving data from api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,33 @@
 import { useRef } from "react";
+import axios from "axios";
 import { Form } from "react-bootstrap";
-
-// const API_URL =
 
 const HomePage = () => {
   const searchRef = useRef(null);
-  //   const typeRef = useRef(null);
   let search = "";
 
-  const options = {
-    method: "GET",
-    url: `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${search}`,
-    headers: {
-      "X-RapidAPI-Key": "cbfa4a4bedmsh6ac359a971f3627p12ba07jsn040823bec505",
-      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
-    },
+  const fetchData = async () => {
+    const options = {
+      method: "GET",
+      url: `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${search}`,
+      headers: {
+        "X-RapidAPI-Key": "cbfa4a4bedmsh6ac359a971f3627p12ba07jsn040823bec505",
+        "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleSearch = (event) => {
     event.preventDefault();
-
     search = searchRef.current.value.trim().replaceAll(" ", "%20");
-    console.log(search);
+    fetchData();
   };
 
   return (
