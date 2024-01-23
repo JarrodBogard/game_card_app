3d822c1ef8f472a4b4714d43dd8239f7efa4b31b
Jarrod Bogard <jarrodbogard@gmail.com>
2024-01-23 09:28:37 -0500

    complete router framework  with react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
 import "./index.css";
 
 function App() {
-  return <div>Hello</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      // element: <RootLayout />,
+      // errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+          children: [
+            {
+              path: "card",
+              // element: <SecondaryLayout />,
+              children: [
+                {
+                  index: true,
+                  // element: <CardPage />
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
